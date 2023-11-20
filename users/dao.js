e11254fd846b6e15157f437f91951ec1a74355a5
unknown <medado.a@northeastern.edu>
2023-11-20 17:04:07 -0500

    Assignment 6 Updates

--- users/dao.js
+++ users/dao.js
@@ -5,28 +5,5 @@ export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) => model.findOne({ username: username });
 export const findUserByCredentials = (usr, pass) => model.findOne({ username: usr, password: pass });
-// export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
-
-export const updateUser = async (userId, user) => {
-    console.log("updateUser");
-    console.log(userId);
-    console.log(user);
-
-    // const result = await model.updateOne({ _id: userId }, { $set: user });
-    // const result = await model.findOne({ _id: userId });
-    // const result = await model.updateOne({ _id: userId });
-    // const result = await model.updateOne({ username: 'ada' }, { $set: user });
-    
-    delete user._id;
-
-    // const result = await model.updateOne({ _id: userId }, { $set: user });
-    
-    const result = await model.updateOne(
-        { _id: '65580756fed6bb3b501c55f2' },
-        { $set: { username: 'ada2' } }
-    );
-    
-    console.log(result);
-}
-
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
